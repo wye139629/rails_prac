2625d85a53f15ae63419bf661f9f5c2dda014feb
will <wye139629@gmail.com>
2021-01-27 22:44:20 +0800

    install carrierwave and add avatar column to book model and modified relative view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_134725) do
+ActiveRecord::Schema.define(version: 2021_01_27_141537) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_01_27_134725) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "ISBN"
+    t.string "avatar"
     t.index ["ISBN"], name: "index_books_on_ISBN"
   end
 
