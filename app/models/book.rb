812a926e7b75b68fd2cc495150aa5dc6763ef80a
will <wye139629@gmail.com>
2021-01-28 01:05:36 +0800

    add validator to books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :ISBN, length: {maximum: 13}
+  validate :check_format
+  def check_format
+    isbn_format = /\d{13}/
+    errors[:ISBN] << "格式不符" unless isbn_format.match(self.ISBN)
+  end
 end
